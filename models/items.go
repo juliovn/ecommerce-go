b4f7104a027f12d294bda176e80fbe41c368c907
Julio Viana Nobre <jviana@thecloudsaviour.com>
2021-09-23 18:52:06 +0200

    added some validation functions to items model

--- models/items.go
+++ models/items.go
@@ -4,8 +4,14 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
+const (
+	ErrUserIDRequired modelError = "models: user ID is required"
+	ErrNameRequired	  modelError = "models: name is required"
+)
+
 type Item struct {
 	gorm.Model
+	UserID 		uint   `gorm:"not_null;index"`
 	Name 		string `gorm:"not_null"`
 	Price		string `gorm:"not_null"`
 	Description string
@@ -33,6 +39,18 @@ type itemValidator struct {
 
 var _ ItemDB = &itemGorm{}
 
+type itemValFn func(*Item) error
+
+func runItemValFns(item *Item, fns ...itemValFn) error {
+	for _, fn := range fns {
+		if err := fn(item); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 func NewItemService(db *gorm.DB) ItemService {
 	return &itemService{
 		ItemDB: &itemValidator{
@@ -46,4 +64,28 @@ func NewItemService(db *gorm.DB) ItemService {
 // Create will create the provided item and backfill data like the ID, CreatedAt, UpdatedAt fields
 func (ig *itemGorm) Create(item *Item) error {
 	return ig.db.Create(item).Error
+}
+func (iv *itemValidator) Create(item *Item) error {
+	err := runItemValFns(item,
+		iv.userIDRequired,
+		iv.nameRequired)
+	if err != nil {
+		return err
+	}
+
+	return iv.ItemDB.Create(item)
+}
+
+func (iv *itemValidator) userIDRequired(i *Item) error {
+	if i.UserID <= 0 {
+		return ErrUserIDRequired
+	}
+	return nil
+}
+
+func (iv *itemValidator) nameRequired(i *Item) error {
+	if i.Name == "" {
+		return ErrNameRequired
+	}
+	return nil
 }
